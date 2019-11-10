7012a8251f76e9a6d31cdfa4af7d3a521c3f69eb
andre farias <andrefariasti@gmail.com>
2019-11-10 16:18:30 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,27 @@
+window.onload = function abas (){
+
+    //var objDiv = document.getElementById('teste');
+    //objDiv.style.color= 'red';
+
+}
+
+var teste;
+
+function validar01(){
+    
+    if(window.event.srcElement.value == teste){
+        alert("Valores repetidos não é prmitido!");
+    }
+
+    teste = window.event.srcElement.value;
+}
+
+function validar02(){
+    
+    if(window.event.srcElement.value == teste){
+        alert("Valores repetidos não é prmitido!");
+        teste = window.event.srcElement.value;
+    }
+
+    teste = window.event.srcElement.value;
+}
