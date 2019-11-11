11ec983e6ec78e92f5dc11e3b63bd2f77463faa8
andre farias <andrefariasti@gmail.com>
2019-11-10 21:36:16 -0300

    Ajuste de linha

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ window.onload = function abas (){
 
     //var objDiv = document.getElementById('teste');
     //objDiv.style.color= 'red';
-
 }
 
 var teste;
@@ -12,7 +11,7 @@ function validar01(){
     if(window.event.srcElement.value == teste){
         alert("Valores repetidos não é prmitido!");
     }
-
+    
     teste = window.event.srcElement.value;
 }
 
